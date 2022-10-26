a1ed8026930b0f349f0def46e27abf069d09df8f
LAPTOP-SERLFI7C\user <m.anas@global-coding.com>
2022-10-26 21:48:53 +0700

    News App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Sidebar from './components/Sidebar';
+import News from './components/News';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  const apiKey = process.env.REACT_APP_API_KEY;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Sidebar key="sidebar" logo={logo} />
+        <Routes>
+          <Route exact path="/" element={<News key="general" category="general" apiKey={apiKey} logo={logo} pageSize={6} country="in" />} ></Route>
+          <Route exact path="/business" element={<News key="business" category="business" apiKey={apiKey} logo={logo} pageSize={6} country="in" />} ></Route>
+          <Route exact path="/science" element={<News key="science" category="science" apiKey={apiKey} logo={logo} pageSize={6} country="in" />} ></Route>
+          <Route exact path="/entertainment" element={<News key="entertainment" category="entertainment" apiKey={apiKey} logo={logo} pageSize={6} country="in" />} ></Route>
+          <Route exact path="/health" element={<News key="health" category="health" apiKey={apiKey} logo={logo} pageSize={6} country="in" />} ></Route>
+          <Route exact path="/sports" element={<News key="sports" category="sports" apiKey={apiKey} logo={logo} pageSize={6} country="in" />} ></Route>
+          <Route exact path="/technology" element={<News key="technology" category="technology" apiKey={apiKey} logo={logo} pageSize={6} country="in" />} ></Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/News.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react'
+import NewsItem from './NewsItem';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import Spinner from './Spinner';
+import PropTypes from 'prop-types'
+
+function News(props) {
+    const [articles, setArticles] = useState([]);
+    const [page, setPage] = useState(0)
+    const [totalResults, setTotalResults] = useState(0)
+    const [isLoading, setLoading] = useState(true);
+    const fetchData = async () => {
+        let res = await fetch(`https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page + 1}&pageSize=${props.pageSize}`);
+        setPage(page + 1)
+        let data = await res.json();
+        setArticles(articles.concat(data.articles));
+        setTotalResults(data.totalResults)
+        setLoading(false)
+    }
+    const capitalize = (string) => {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+    useEffect(() => {
+        document.title = `React News - ${capitalize(props.category)}`
+        fetchData()
+    })
+
+    return (
+        <section className="min-h-screen pt-2 pl-2 pr-2 sm:pl-64 bg-gray-800">
+            <div className="text-xl sm:text-2xl md:text-3xl font-semibold pb-3 pl-5 mt-3 border border-transparent border-b-yellow-400 text-gray-200">Top headlines - {capitalize(props.category)}</div>
+            <div className="grid grid-cols-1 lg:grid-cols-2">
+                {articles.map((article) => {
+                    return <NewsItem key={article.url} title={article.title} description={article.description} author={article.author} image={article.urlToImage} source={article.source.name} url={article.url} date={article.publishedAt} />
+                })}
+            </div>
+            {isLoading && <Spinner />}
+            <InfiniteScroll
+                dataLength={articles.length} //This is important field to render the next data
+                next={fetchData}
+                hasMore={articles.length !== totalResults}
+                loader={<Spinner />} >
+            </InfiniteScroll>
+        </section>
+
+    )
+}
+News.propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string
+}
+News.defaultProps = {
+    country: "in",
+    pageSize: 6,
+    category: "general"
+}
+export default News
